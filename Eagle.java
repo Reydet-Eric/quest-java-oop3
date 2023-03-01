198e210935b9beceda63db200f35b832d52534f0
Reydet-Eric <eric.reydet@sncf.fr>
2023-03-01 12:05:41 +0100

    ok

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -13,6 +13,10 @@ public class Eagle extends Bird {
         return altitude;
     }
 
+    public void setAltitude(int meters) {
+        this.altitude = this.altitude + meters;
+            }
+
     public boolean isFlying() {
         return flying;
     }
@@ -21,4 +25,35 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+    @Override
+    public void takeOff() {
+        System.out.println("Eye Cherry takes off in the sky.");
+    }
+
+   @Override
+    public void ascend(int meters) {
+        this.setAltitude(meters);
+            System.out.println("Eye Cherry flies upward, altitude : "+ getAltitude());
+      }
+
+ @Override
+    public void descend(int meters) {
+        meters = - meters;
+        this.setAltitude(meters);
+            System.out.println("Eye Cherry flies downward, altitude : "+ getAltitude());
+      }
+
+
+@Override
+    public void glide() {
+        System.out.println("It glides into the air.");
+    }
+@Override
+    public void land() {
+        if (getAltitude()>1) {
+            System.out.println("Eye Cherry is too high, it can't lands.") ;
+        } else {
+        System.out.println("Eye Cherry lands on the ground.");}
+    }
+
 }
